91055bee0fd46a79e0dbc9c32708dbe02d304752
Sabareesh Subramaniam <sabareeshs@gmail.com>
2020-05-20 20:14:15 -0700

    Database

--- codenames/settings.py
+++ codenames/settings.py
@@ -11,12 +11,19 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 import os
-import django_heroku
+import dotenv
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 STATIC_DIR = os.path.join(BASE_DIR, 'static')
 TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
+
+dotenv_file = os.path.join(BASE_DIR, ".env")
+if os.path.isfile(dotenv_file):
+    dotenv.load_dotenv(dotenv_file)
+else:
+	import django_heroku
 
 
 # Quick-start development settings - unsuitable for production
@@ -77,12 +84,14 @@ WSGI_APPLICATION = 'codenames.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#    }
+#}
+DATABASES = {}
+DATABASES['default'] = dj_database_url.config(conn_max_age=600)
 
 
 # Password validation
@@ -125,5 +134,9 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [STATIC_DIR, ]
 
-# Activate Django-Heroku.
-django_heroku.settings(locals())
+if not os.path.isfile(dotenv_file):
+	# Activate Django-Heroku.
+	django_heroku.settings(locals())
+#else:
+	# This is new
+	#del DATABASES['default']['OPTIONS']['sslmode']
